f769444ce3ec0d0c15744c1d37262f929b9ac43e
andyjefferson <andy@datanucleus.org>
2014-02-28 16:38:43 +0000

    more supportedOptions

--- src/java/org/datanucleus/store/hbase/HBaseStoreManager.java
+++ src/java/org/datanucleus/store/hbase/HBaseStoreManager.java
@@ -110,6 +110,7 @@ public class HBaseStoreManager extends AbstractStoreManager implements SchemaAwa
         set.add(StoreManager.OPTION_DATASTORE_ID);
         set.add(StoreManager.OPTION_NONDURABLE_ID);
         set.add(StoreManager.OPTION_ORM);
+        set.add(StoreManager.OPTION_ORM_EMBEDDED_PC);
         set.add(StoreManager.OPTION_TXN_ISOLATION_READ_COMMITTED);
         return set;
     }
