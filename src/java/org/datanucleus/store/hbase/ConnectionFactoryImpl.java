7b18ddc9e7d7238b50f9b8a7fceb63104f01c1f9
andyjefferson <andy@datanucleus.org>
2014-03-06 09:21:38 +0000

    fix param name

--- src/java/org/datanucleus/store/hbase/ConnectionFactoryImpl.java
+++ src/java/org/datanucleus/store/hbase/ConnectionFactoryImpl.java
@@ -108,10 +108,10 @@ public class ConnectionFactoryImpl extends AbstractConnectionFactory
      * Obtain a connection from the Factory. The connection will be enlisted within the transaction
      * associated to the ExecutionContext
      * @param ec the pool that is bound the connection during its lifecycle (or null)
-     * @param txnOptionsIgnored Any options for creating the connection
+     * @param options Any options for creating the connection
      * @return the {@link org.datanucleus.store.connection.ManagedConnection}
      */
-    public ManagedConnection createManagedConnection(ExecutionContext ec, Map txnOptionsIgnored)
+    public ManagedConnection createManagedConnection(ExecutionContext ec, Map options)
     {
         HBaseManagedConnection managedConnection = connectionPool.getPooledConnection();
         if (managedConnection == null) 
