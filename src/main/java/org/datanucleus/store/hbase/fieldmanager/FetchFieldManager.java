04436aa1bd8f5262ab5c5937133e6a0edb7acb41
andyjefferson <andy@datanucleus.org>
2016-09-19 16:34:42 +0100

    Support core-134. Fixes #27

--- src/main/java/org/datanucleus/store/hbase/fieldmanager/FetchFieldManager.java
+++ src/main/java/org/datanucleus/store/hbase/fieldmanager/FetchFieldManager.java
@@ -59,7 +59,6 @@ import org.datanucleus.store.schema.table.Table;
 import org.datanucleus.store.types.SCOUtils;
 import org.datanucleus.store.types.converters.MultiColumnConverter;
 import org.datanucleus.store.types.converters.TypeConverter;
-import org.datanucleus.store.types.converters.TypeConverterHelper;
 import org.datanucleus.util.NucleusLogger;
 import org.datanucleus.util.TypeConversionHelper;
 
@@ -815,7 +814,7 @@ public class FetchFieldManager extends AbstractFetchFieldManager
                     String familyName = HBaseUtils.getFamilyNameForColumn(col);
                     String qualifName = HBaseUtils.getQualifierNameForColumn(col);
 
-                    Class datastoreType = TypeConverterHelper.getDatastoreTypeForTypeConverter(conv, mmd.getType());
+                    Class datastoreType = ec.getTypeManager().getDatastoreTypeForTypeConverter(conv, mmd.getType());
                     byte[] bytes = result.getValue(familyName.getBytes(), qualifName.getBytes());
                     if (datastoreType == byte[].class)
                     {
